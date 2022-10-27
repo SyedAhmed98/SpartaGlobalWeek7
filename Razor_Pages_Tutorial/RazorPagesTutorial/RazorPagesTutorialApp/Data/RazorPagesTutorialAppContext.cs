3cf687bf22e74a6742e617b1b3b6e86db44d8d61
Syed Ahmed <nadir.ahmed@live.co.uk>
2022-10-27 11:52:56 +0100

    completed part 2

---
+++ Razor_Pages_Tutorial/RazorPagesTutorial/RazorPagesTutorialApp/Data/RazorPagesTutorialAppContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesTutorialApp.Models;
+
+namespace RazorPagesTutorialApp.Data
+{
+    public class RazorPagesTutorialAppContext : DbContext
+    {
+        public RazorPagesTutorialAppContext (DbContextOptions<RazorPagesTutorialAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RazorPagesTutorialApp.Models.Movie> Movie { get; set; } = default!;
+    }
+}
